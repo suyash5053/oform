1f4c3a88ae25249123fc6471dcffa34ef4cee0d0
Suyash Srivastava <suyashsrivastava5053@gmail.com>
2023-10-05 19:11:52 +0530

    Created cards to show Created Forms

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,20 @@
 import {GetFormStats} from "@/actions/form";
 import {LuView} from "react-icons/lu";
 import {ReactNode, Suspense} from "react";
-import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
 import {Skeleton} from "@/components/ui/skeleton";
 import {Separator} from "@/components/ui/separator";
-import {FaWpforms} from "react-icons/fa";
+import {FaEdit, FaWpforms} from "react-icons/fa";
 import {HiCursorClick} from "react-icons/hi";
 import {TbArrowBounce} from "react-icons/tb";
 import CreateFormBtn from "@/components/CreateFormBtn";
+import {GetForms} from "@/actions/form";
+import {Form} from "@prisma/client";
+import {Badge} from "@/components/ui/badge";
+import {formatDistance} from "date-fns";
+import {BiRightArrowAlt} from "react-icons/bi";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
     return (
@@ -18,7 +25,17 @@ export default function Home() {
          <Separator className={"my-6"} />
          <h2 className={"text-4xl font-bold col-span-2"}>Forms</h2>
          <Separator className={"my-6"} />
-         <CreateFormBtn />
+         <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"}>
+             <CreateFormBtn />
+                <Suspense
+                    // @ts-ignore
+                    fallback ={[1,2,3,4].map((el) => {
+                        <FormCardSkeleton key={el} />
+                    })}
+                >
+                    <FormCards />
+                </Suspense>
+         </div>
      </div>
     );
 }
@@ -91,13 +108,76 @@ function StatsCard({title, icon, helpText, value, loading, className}:{
                 <div className={"text-2xl font-bold"}>
                     {
                         loading && <Skeleton>
-                            <span className={"opacity-0"}>0</span>
+                            <span className={"opacity-0"}>0
+                            </span>
                         </Skeleton>
                     }
                     {!loading && value}
                 </div>
-                <p className={"text-xs text-muted-foreground pt-1"}>{helpText}</p>
+                <p className={"text-xs text-muted-foreground pt-1"}>
+                    {helpText}
+                </p>
             </CardContent>
+        </Card>
+    );
+}
+
+function FormCardSkeleton() {
+    return <Skeleton className={"border-2 border-primary-/20 h-[190px] w-full"} />;
+}
+
+async function FormCards() {
+    const forms = await GetForms();
+    return (
+        <>
+            {forms.map((form) => (
+                <FormCard key={form.id} form={form} />
+            ))}
+        </>
+    );
+}
+
+function FormCard({form}: {form: Form}) {
+    return (
+        <Card className={""}>
+            <CardHeader className={""}>
+                <CardTitle className={"flex items-center gap-2 justify-between"}>
+                    <span className={"truncate font-bold"}>{form.name}
+                    </span>
+                    {form.published && <Badge>Published</Badge>}
+                    {!form.published && <Badge variant={"destructive"}>Draft</Badge>}
+                </CardTitle>
+                <CardDescription className={"flex items-center justify-between text-muted-foreground text-sm"}>
+                    {formatDistance(form.createdAt, new Date(), {addSuffix: true})}
+                    {form.published && (
+                        <span className={"flex items-center gap-2"}>
+                            <LuView className={"text-muted-foreground"} />
+                            <span>{form.visits.toLocaleString()}</span>
+                            <FaWpforms className={"text-muted-foreground"} />
+                            <span>{form.submissions.toLocaleString()}</span>
+                        </span>)
+                    }
+                </CardDescription>
+            </CardHeader>
+            <CardContent className={"h-[20px] truncate text-sm text-muted-foreground"}>
+                {form.description || "No available description"}
+            </CardContent>
+            <CardFooter>
+                {form.published && (
+                    <Button asChild className={"w-full mt-2 text-md gap-4"}>
+                        <Link href={`/form/${form.id}`}>
+                            View Submissions <BiRightArrowAlt />
+                        </Link>
+                    </Button>
+                )}
+                {!form.published && (
+                    <Button asChild variant={"secondary"} className={"w-full mt-2 text-md gap-4"}>
+                        <Link href={`/builder/${form.id}`}>
+                            Edit Form<FaEdit />
+                        </Link>
+                    </Button>
+                )}
+            </CardFooter>
         </Card>
     );
 }
--- components/CreateFormBtn.tsx
+++ components/CreateFormBtn.tsx
@@ -12,8 +12,11 @@ import {Textarea} from "@/components/ui/textarea";
 import {toast} from "@/components/ui/use-toast";
 import { formSchema, formSchemaType } from "@/schemas/form";
 import {CreateForm} from "@/actions/form";
+import { BsFileEarmarkPlus } from "react-icons/bs";
+import {useRouter} from "next/navigation";
 
 function CreateFormBtn() {
+    const router = useRouter();
     const form = useForm<formSchemaType>({
         resolver: zodResolver(formSchema),
     });
@@ -25,7 +28,7 @@ function CreateFormBtn() {
                 title: "Success",
                 description: "Form created successfully",
             });
-            console.log("Form ID", formId);
+            router.push(`/builder/${formId}`);
         }
         catch (error) {
          toast({
@@ -39,7 +42,15 @@ function CreateFormBtn() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <Button>Create New Form</Button>
+                <Button
+                    variant={"outline"}
+                    className={"group border border-primary/20 h-[190px] items-center justify-center flex flex-col hover:border-primary" +
+                    "hover:cursor-pointer border-dashed gap-4"}>
+                    <BsFileEarmarkPlus className={"h-8 w-8 text-muted-foreground group-hover:text-primary"} />
+                    <p className={"font-bold text-xl text-muted-foreground group-hover:text-primary"}>
+                    Create New Form
+                    </p>
+                </Button>
             </DialogTrigger>
             <DialogContent>
                 <DialogHeader>
